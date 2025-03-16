af2572c3829573a56237f382a255349a448c9f6b
bone1nis <bone1nis@gmail.com>
2025-03-16 13:30:16 +0300

    chore(zustand) feat(header, footer)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import Image from "next/image";
+"use client";
+import { useBearsStore } from "@/stores/bears-store";
 
 export default function Home() {
+  const bears = useBearsStore(state => state.bears);
+  const increasePopulation = useBearsStore(state => state.increasePopulation);
+
+  const onBtnClick = () => {
+    increasePopulation();
+  }
   return (
-    <div>12</div>
+    <div>
+      <p>Bears: {bears}</p>
+      <button onClick={onBtnClick}>2</button>
+    </div>
   );
 }
