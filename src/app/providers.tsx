74d70c271ff5184c324faacce2369c0fc3975d2d
bone1nis <bone1nis@gmail.com>
2025-03-25 14:26:11 +0300

    feat(providers): progress-bar and feat(FilmItem): small props

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,14 +2,21 @@
 import { QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { getQueryClient } from "@/helpers/getQueryClient"
+import { ProgressProvider } from "@bprogress/next/app"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient()
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <ProgressProvider
+      height="3px"
+      color="#F3A505"
+      options={{ showSpinner: false }}
+      >
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ProgressProvider>
   )
 }
--- src/components/FilmItem.tsx
+++ src/components/FilmItem.tsx
@@ -6,23 +6,40 @@ import { useRouter } from 'next/navigation';
 
 type FilmItemProps = {
     film: Movie;
+    small?: boolean;
 }
 
-export const FilmItem: React.FC<FilmItemProps> = ({ film }) => {
+export const FilmItem: React.FC<FilmItemProps> = ({ film, small }) => {
     const router = useRouter();
+
+    const formatedFilmName = film.name.length > 30 ? film.name.slice(0, 31) + "..." : film.name;
+
+    const imageSize = small ? 100 : 150;
+    const height = small ? 150 : 225;
+    const containerClass = small
+        ? "w-[200px] h-[280px] p-2"
+        : "w-full h-[320px] p-3";
+
+    const fontSize = small ? "text-sm" : "text-lg";
+    const titleClass = small ? "text-base" : "text-lg";
+
     return (
-        <div 
-        className="w-full h-full bg-white shadow-md rounded-lg overflow-hidden p-3 flex flex-col items-center text-center transition-transform hover:scale-105 cursor-pointer"
-        onClick={() => router.push(`/film/${film.id}`)}>
-            <Image
-                src={film.poster.url}
-                alt={`Постер ${film.name}`}
-                width={150}
-                height={225}
-                className="w-[150px] h-[225px] object-cover rounded-md"
-            />
-            <h5 className="font-bold text-lg mt-2">{film.name}</h5>
-            <p className="text-gray-600 text-sm">({film.year})</p>
+        <div
+            className={`bg-white shadow-md rounded-lg overflow-hidden flex flex-col items-center text-center transition-transform hover:scale-105 cursor-pointer ${containerClass}`}
+            onClick={() => router.push(`/film/${film.id}`)}>
+            
+            <div className={`relative ${small ? 'w-[100px] h-[150px]' : 'w-[150px] h-[225px]'}`}>
+                <Image
+                    src={film.poster.url}
+                    alt={`Постер ${film.name}`}
+                    width={imageSize}
+                    height={height}
+                    className="object-cover rounded-md"
+                />
+            </div>
+
+            <h5 className={`font-bold mt-2 ${titleClass}`}>{formatedFilmName}</h5>
+            <p className={`text-gray-600 ${fontSize}`}>({film.year})</p>
         </div>
     );
 }
